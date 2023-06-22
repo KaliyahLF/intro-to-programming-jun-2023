f3a13704a8a931204999d251ac5ea69b40d013ac
Kaliyah Fisher <kaliyah_fisher@Progressive.com>
2023-06-22 11:45:00 -0400

    gold customer

--- BankingSolution/BankingUnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
+++ BankingSolution/BankingUnitTests/BankAccount/GoldCustomersGetABonusOnDeposits.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BankingUnitTests.BankAccount;
-internal class GoldCustomersGetABonusOnDeposits
+public class GoldCustomersGetABonusOnDeposits
 {
+    [Fact]
+    public void BonusIsApplied()
+    {
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+        account.AccountType = LoyaltyLevel.Gold;
+        var amountToDeposit =  100M;
+        var expectedNewBalance = openingBalance + amountToDeposit;
+
+        account.Deposit(amountToDeposit);
+        Assert.Equal(expectedNewBalance, account.GetBalance());
+    }
 }
