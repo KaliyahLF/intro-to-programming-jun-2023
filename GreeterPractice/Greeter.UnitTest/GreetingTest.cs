15029b1221230ae482cf62707dd4c6099886caa9
Kaliyah Fisher <kaliyah_fisher@Progressive.com>
2023-06-23 14:44:34 -0400

    updates for Friday

---
+++ GreeterPractice/Greeter.UnitTest/GreetingTest.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Xml.Linq;
+
+namespace Greeter.UnitTest;
+public class GreetingTest
+{
+    [Theory]
+    [InlineData("Windom", "Hello, Windom!")]
+    [InlineData("Cooper", "Hello, Cooper!")]
+    [InlineData("Kaliyah", "Hello, Kaliyah!")]
+    public void SingleName(string name, string expected)
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("", "Hello, Captain!")]
+    public void EmptyName(string name, string expected)
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("WINDOM", "HELLO, WINDOM!")]
+    [InlineData("COOPER", "HELLO, COOPER!")]
+    [InlineData("KALIYAH", "HELLO, KALIYAH!")]
+    public void ShoutName(string name, string expected)
+    {
+        var greeter = new GreetingMaker();
+
+        string greeting = greeter.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+
+
+
+    [Theory]
+    [InlineData("Bob", "Mary", "Hello, Bob and Mary!")]
+    [InlineData("Jalen", "Kaliyah", "Hello, Jalen and Kaliyah!")]
+    [InlineData("Anakin", "Kenobi", "Hello, Anakin and Kenobi!")]
+    public void MultipleNames(string name1, string name2, string expected)
+    {
+        var greeter = new GreetingMaker();
+        string greeting = greeter.Greet(name1, name2);
+
+        Assert.Equal(expected, greeting);
+
+    }
+}
---
+++ GreeterPractice/Greeter/GreetingMaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Greeter
+{
+    public class GreetingMaker
+    {
+        private readonly List<String> _names = new();
+
+        
+        public string Greet(string? name)
+        {
+            if (name == "") { name = "Captain"; }
+            if (name == name.ToUpper()) { return "HELLO, " + name + "!"; }
+
+            return "Hello, " + name + "!";
+        }
+
+        public string Greet(string? name1, string? name2) {
+            if (name1=="") { name1 = "Captain"; }
+            if (name2=="") { name2 = "Captain"; }
+            if (name1 == name1.ToUpper()) { return "Hello, " + name2 
+                    + " AND " + name1 +"!"; }
+            if (name2 == name2.ToUpper()) { return "Hello, " + name1
+                    + " AND " + name2 + "!"; }
+
+            return "Hello, " + name1 + " and " + name2 + "!";
+        }
+    }
+}
